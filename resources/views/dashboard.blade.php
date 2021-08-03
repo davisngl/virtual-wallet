cac1f64019da12b0a463ba077725391c8eeac5c3
Dāvis <davisnaglis2@gmail.com>
2021-08-03 10:37:37 +0300

    Base dashboard view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,31 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <div class="action-list flex flex-row justify-between">
+                        <div class="w-1/4 py-2 px-5 border-green-800 bg-green-200">
+                            <p class="text-2xl font-bold pb-5">Wallets</p>
+                            <ul class="actions list-disc px-5">
+                                <li><a href="#" class="hover:underline">Create a wallet</a></li>
+                                <li><a href="#" class="hover:underline">Show all wallets</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="w-1/4 py-2 px-5 border-blue-800 bg-blue-200">
+                            <p class="text-2xl font-bold pb-5">Transactions</p>
+                            <ul class="actions list-disc px-5">
+                                <li><a href="#" class="hover:underline">"EUR" wallet transaction list</a></li>
+                                <li><a href="#" class="hover:underline">"USD" wallet transaction list</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="w-1/4 py-2 px-5 border-yellow-800 bg-yellow-200">
+                            <p class="text-2xl font-bold pb-5">Payments & wallet status</p>
+                            <ul class="actions list-disc px-5">
+                                <li><a href="#" class="hover:underline">Make a transaction</a></li>
+                                <li><a href="#" class="hover:underline">Balance view</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

81c1e45447b96d0864f042121271fab4d799e301
Dāvis <davisnaglis2@gmail.com>
2021-08-03 10:24:04 +0300

    Clear up routing for simplicity, change redirect url

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+   Route::view('/', 'dashboard')->name('dashboard');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
